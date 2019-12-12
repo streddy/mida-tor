6682fcff0b6a34dfb2e3af88f35860127539e234
Sanjeev Reddy <streddy2@illinois.edu>
2019-12-12 23:35:11 +0000

    mida over tor

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM ubuntu
 
 RUN apt-get update && apt-get -y upgrade && apt-get -y install \
-  python3 ca-certificates chromium-browser tcpdump
+  python3 ca-certificates chromium-browser tcpdump apt-utils software-properties-common
+
+RUN apt-get -y install wget
+RUN apt-get -y install curl
+RUN apt -y install apt-transport-https && add-apt-repository universe
+RUN wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | apt-key add -
+RUN echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | tee -a /etc/apt/sources.list
+RUN apt update && apt -y install tor deb.torproject.org-keyring
 
 COPY setup.py /root
 
--- pcap-script.sh
+++ pcap-script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+service tor start
+curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs
+
 tcpdump -i eth0 -w /trace.pcap & 
 
 export SSLKEYLOGFILE=/ssl.log
 
-/usr/local/bin/mida go --add-browser-flags=headless,disable-gpu $@
-
+#/usr/local/bin/mida go --add-browser-flags=headless,disable-gpu $@
+/usr/local/bin/mida go --add-browser-flags=headless,disable-gpu,remote-debugging-port=9222,proxy-server="socks5://localhost:9050" --completion=CompleteOnLoadEvent --timeout=60 $@
+#chromium-browser --no-sandbox --headless --remote-debugging-port=9222 --disable-gpu --proxy-server=“socks5://localhost:9050” $@ &
+#sleep 2 &
 kill -1 %%
 
 sleep 3
